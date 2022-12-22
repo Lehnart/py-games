15d5a3c92d39120436495ce3bd732ba01c3bb2ce
Lehnart <thomasserre63@gmail.com>
2022-12-22 10:10:20 +0100

    correct code smell

--- tests/test_engine/test_mesper.py
+++ tests/test_engine/test_mesper.py
@@ -8,16 +8,18 @@ class TestMessageQueue:
     class UnknownEvent(Event):
         pass
 
+    test_message_str_1 = "Test message 1"
+
     def test_add(self):
         msg_queue = MessageQueue()
-        msg_queue.add(TestMessageQueue.AnEvent, "Test message 1")
+        msg_queue.add(TestMessageQueue.AnEvent, TestMessageQueue.test_message_str_1)
         msg_queue.add(TestMessageQueue.AnEvent, "Test message 2")
         assert len(msg_queue.get(TestMessageQueue.AnEvent)) == 2
         assert len(msg_queue.get(TestMessageQueue.UnknownEvent)) == 0
 
     def test_get(self):
         msg_queue = MessageQueue()
-        msg_queue.add(TestMessageQueue.AnEvent, "Test message 1")
+        msg_queue.add(TestMessageQueue.AnEvent, TestMessageQueue.test_message_str_1)
         assert len(msg_queue.get(TestMessageQueue.AnEvent)) == 1
-        assert msg_queue.get(TestMessageQueue.AnEvent) == ["Test message 1"]
+        assert msg_queue.get(TestMessageQueue.AnEvent) == [TestMessageQueue.test_message_str_1]
         assert msg_queue.get(TestMessageQueue.UnknownEvent) == []
