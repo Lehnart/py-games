d8bd2ff3ea72d0dd5f1a0eed0737d3159184b62a
Lehnart <thomasserre63@gmail.com>
2022-12-22 10:04:05 +0100

    add test for messagequeue get

--- tests/test_engine/test_mesper.py
+++ tests/test_engine/test_mesper.py
@@ -14,3 +14,10 @@ class TestMessageQueue:
         msg_queue.add(TestMessageQueue.AnEvent, "Test message 2")
         assert len(msg_queue.get(TestMessageQueue.AnEvent)) == 2
         assert len(msg_queue.get(TestMessageQueue.UnknownEvent)) == 0
+
+    def test_get(self):
+        msg_queue = MessageQueue()
+        msg_queue.add(TestMessageQueue.AnEvent, "Test message 1")
+        assert len(msg_queue.get(TestMessageQueue.AnEvent)) == 1
+        assert msg_queue.get(TestMessageQueue.AnEvent) == ["Test message 1"]
+        assert msg_queue.get(TestMessageQueue.UnknownEvent) == []
