32f51f92de9769cde5f31220fcadea9ccb550a99
Lehnart <thomasserre63@gmail.com>
2023-01-07 19:05:22 +0100

    simplify hauler updater

--- colony_builder/settlers/processors/hauler_updater.py
+++ colony_builder/settlers/processors/hauler_updater.py
@@ -19,74 +19,94 @@ class HaulerUpdater(Processor):
 
     def process(self):
         haulers = self.world.get_components(Hauler, Agent)
-        for hauler_ent, [hauler_comp, hauler_agent] in haulers:
+        for hauler_ent, [hauler_comp, agent_comp] in haulers:
             path_comp = self.world.component_for_entity(hauler_comp.path_ent, Path)
-            mean_pos = self.compute_path_mean_position(path_comp)
-            dist = math.sqrt((hauler_agent.pos[0] - mean_pos[0]) ** 2 + (hauler_agent.pos[1] - mean_pos[1]) ** 2)
 
             if hauler_comp.state == Hauler.State.IDLE:
-                if dist > self.distance_tolerance:
-                    hauler_comp.state = Hauler.State.MOVING_TO_PATH
-                else:
-                    flags_and_resources = self.get_flag_with_resource_to_pick(path_comp)
-                    if not flags_and_resources:
-                        continue
+                self.process_idle_state(hauler_comp, agent_comp, path_comp)
 
-                    for flag, resource in flags_and_resources:
-                        resource_comp = self.world.component_for_entity(resource, Resource)
-                        if (
-                                (flag == path_comp.flag1_ent and resource_comp.next_flag == path_comp.flag2_ent) or
-                                (flag == path_comp.flag2_ent and resource_comp.next_flag == path_comp.flag1_ent)
-                        ):
-                            hauler_comp.state = Hauler.State.MOVING_TO_PICK
-                            hauler_comp.flag_destination = flag
-                            hauler_comp.resource_to_pick = resource
-                            break
+            elif hauler_comp.state == Hauler.State.MOVING_TO_PATH:
+                self.process_moving_to_path(hauler_ent, hauler_comp, agent_comp, path_comp)
 
-            if hauler_comp.state == Hauler.State.MOVING_TO_PATH:
-                if dist < self.distance_tolerance:
-                    hauler_comp.state = Hauler.State.IDLE
-                else:
-                    self.world.publish(MoveAgent(hauler_ent, mean_pos))
+            elif hauler_comp.state == Hauler.State.MOVING_TO_PICK:
+                self.process_moving_to_pick_state(hauler_ent, hauler_comp, agent_comp)
 
-            if hauler_comp.state == Hauler.State.MOVING_TO_PICK:
-                flag_pos = self.compute_flag_mean_position(hauler_comp.flag_destination)
-                dist = math.sqrt((hauler_agent.pos[0] - flag_pos[0]) ** 2 + (hauler_agent.pos[1] - flag_pos[1]) ** 2)
-                if dist < self.distance_tolerance:
-                    hauler_comp.state = Hauler.State.PICKING_RESOURCE
-                else:
-                    self.world.publish(MoveAgent(hauler_ent, flag_pos))
+            elif hauler_comp.state == Hauler.State.PICKING_RESOURCE:
+                self.process_picking_resource_state(hauler_comp, path_comp)
 
-            if hauler_comp.state == Hauler.State.PICKING_RESOURCE:
-                self.world.remove_component(hauler_comp.resource_to_pick, Sprite)
-                self.world.remove_component(hauler_comp.resource_to_pick, GridPosition)
-                if hauler_comp.flag_destination == path_comp.flag2_ent:
-                    hauler_comp.flag_destination = path_comp.flag1_ent
-                else:
-                    hauler_comp.flag_destination = path_comp.flag2_ent
-                hauler_comp.state = Hauler.State.MOVING_TO_DROP
+            elif hauler_comp.state == Hauler.State.MOVING_TO_DROP:
+                self.process_moving_to_drop(hauler_ent, hauler_comp, agent_comp)
 
-            if hauler_comp.state == Hauler.State.MOVING_TO_DROP:
-                flag_pos = self.compute_flag_mean_position(hauler_comp.flag_destination)
-                dist = math.sqrt((hauler_agent.pos[0] - flag_pos[0]) ** 2 + (hauler_agent.pos[1] - flag_pos[1]) ** 2)
-                if dist < self.distance_tolerance:
-                    hauler_comp.state = Hauler.State.DROPPING_RESOURCE
-                else:
-                    self.world.publish(MoveAgent(hauler_ent, flag_pos))
+            elif hauler_comp.state == Hauler.State.DROPPING_RESOURCE:
+                self.process_dropping_resource(hauler_comp, agent_comp)
 
-            if hauler_comp.state == Hauler.State.DROPPING_RESOURCE:
-                gpx, gpy = int(hauler_agent.pos[0]), int(hauler_agent.pos[1])
-                self.world.add_component(hauler_comp.resource_to_pick,
-                                         Sprite(config.WOOD_SURFACE, (gpx * 16, gpy * 16)))
-                self.world.add_component(hauler_comp.resource_to_pick, GridPosition(gpx, gpy))
+    def process_idle_state(self, hauler_comp: Hauler, agent_comp: Agent, path_comp: Path):
 
-                resource_comp = self.world.component_for_entity(hauler_comp.resource_to_pick, Resource)
-                resource_comp.next_flag = None
-                resource_comp.current_flag = hauler_comp.flag_destination
+        mean_pos = self.compute_path_mean_position(path_comp)
+        dist = math.sqrt((agent_comp.pos[0] - mean_pos[0]) ** 2 + (agent_comp.pos[1] - mean_pos[1]) ** 2)
+        if dist > self.distance_tolerance:
+            hauler_comp.state = Hauler.State.MOVING_TO_PATH
+        else:
+            flags_and_resources = self.get_flag_with_resource_to_pick(path_comp)
+            if not flags_and_resources:
+                return
 
-                hauler_comp.flag_destination = None
-                hauler_comp.resource_to_pick = None
-                hauler_comp.state = Hauler.State.IDLE
+            for flag, resource in flags_and_resources:
+                resource_comp = self.world.component_for_entity(resource, Resource)
+                if (
+                        (flag == path_comp.flag1_ent and resource_comp.next_flag == path_comp.flag2_ent) or
+                        (flag == path_comp.flag2_ent and resource_comp.next_flag == path_comp.flag1_ent)
+                ):
+                    hauler_comp.state = Hauler.State.MOVING_TO_PICK
+                    hauler_comp.flag_destination = flag
+                    hauler_comp.resource_to_pick = resource
+                    break
+
+    def process_moving_to_path(self, ent: int, hauler_comp: Hauler, agent_comp: Agent, path_comp: Path):
+        mean_pos = self.compute_path_mean_position(path_comp)
+        dist = math.sqrt((agent_comp.pos[0] - mean_pos[0]) ** 2 + (agent_comp.pos[1] - mean_pos[1]) ** 2)
+
+        if dist < self.distance_tolerance:
+            hauler_comp.state = Hauler.State.IDLE
+        else:
+            self.world.publish(MoveAgent(ent, mean_pos))
+
+    def process_moving_to_pick_state(self, ent: int, hauler_comp: Hauler, agent_comp: Agent):
+        is_close, flag_pos = self.check_distance_to_destination_flag(hauler_comp, agent_comp)
+        if is_close:
+            hauler_comp.state = Hauler.State.PICKING_RESOURCE
+        else:
+            self.world.publish(MoveAgent(ent, flag_pos))
+
+    def process_picking_resource_state(self, hauler_comp: Hauler, path_comp: Path):
+        self.world.remove_component(hauler_comp.resource_to_pick, Sprite)
+        self.world.remove_component(hauler_comp.resource_to_pick, GridPosition)
+        if hauler_comp.flag_destination == path_comp.flag2_ent:
+            hauler_comp.flag_destination = path_comp.flag1_ent
+        else:
+            hauler_comp.flag_destination = path_comp.flag2_ent
+        hauler_comp.state = Hauler.State.MOVING_TO_DROP
+
+    def process_moving_to_drop(self, ent: int, hauler_comp: Hauler, agent_comp: Agent):
+        is_close, flag_pos = self.check_distance_to_destination_flag(hauler_comp, agent_comp)
+        if is_close:
+            hauler_comp.state = Hauler.State.DROPPING_RESOURCE
+        else:
+            self.world.publish(MoveAgent(ent, flag_pos))
+
+    def process_dropping_resource(self, hauler_comp: Hauler, agent_comp: Agent):
+        gpx, gpy = int(agent_comp.pos[0]), int(agent_comp.pos[1])
+        self.world.add_component(hauler_comp.resource_to_pick,
+                                 Sprite(config.WOOD_SURFACE, (gpx * 16, gpy * 16)))
+        self.world.add_component(hauler_comp.resource_to_pick, GridPosition(gpx, gpy))
+
+        resource_comp = self.world.component_for_entity(hauler_comp.resource_to_pick, Resource)
+        resource_comp.next_flag = None
+        resource_comp.current_flag = hauler_comp.flag_destination
+
+        hauler_comp.flag_destination = None
+        hauler_comp.resource_to_pick = None
+        hauler_comp.state = Hauler.State.IDLE
 
     def get_flag_with_resource_to_pick(self, path_comp: Path) -> List[Tuple[int, int]]:
         flags_and_resources = []
@@ -99,12 +119,17 @@ class HaulerUpdater(Processor):
                 flags_and_resources.append((flag2, resource_ent))
         return flags_and_resources
 
+    def check_distance_to_destination_flag(self, hauler_comp: Hauler, agent_comp: Agent):
+        flag_pos = self.compute_flag_mean_position(hauler_comp.flag_destination)
+        dist = math.sqrt((agent_comp.pos[0] - flag_pos[0]) ** 2 + (agent_comp.pos[1] - flag_pos[1]) ** 2)
+        return dist < self.distance_tolerance
+
+    def compute_flag_mean_position(self, flag: int) -> Tuple[float, float]:
+        grid_pos_comp = self.world.component_for_entity(flag, GridPosition)
+        return grid_pos_comp.pos[0] + 0.5, grid_pos_comp.pos[1] + 0.5
+
     def compute_path_mean_position(self, path_comp: Path) -> Tuple[float, float]:
         road_ents = path_comp.road_ents
         grid_pos_comps = [self.world.component_for_entity(road_ent, GridPosition) for road_ent in road_ents]
         gpxs, gpys = [gpc.pos[0] + 0.5 for gpc in grid_pos_comps], [gpc.pos[1] + 0.5 for gpc in grid_pos_comps]
         return sum(gpxs) / len(gpxs), sum(gpys) / len(gpys)
-
-    def compute_flag_mean_position(self, flag: int) -> Tuple[float, float]:
-        grid_pos_comp = self.world.component_for_entity(flag, GridPosition)
-        return grid_pos_comp.pos[0] + 0.5, grid_pos_comp.pos[1] + 0.5
