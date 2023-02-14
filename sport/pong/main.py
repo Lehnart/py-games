1b2a02f534be35b8a3ba9d8e631655a8942c6b94
Lehnart <thomasserre63@gmail.com>
2023-02-14 14:42:35 +0100

    Allow paddle to move

--- sport/pong/main.py
+++ sport/pong/main.py
@@ -1,10 +1,15 @@
+import pygame
+
+from mesper.components.keyboard_input import KeyboardInput
 from mesper.components.rectangle import Rectangle
 from mesper.components.window import Window
+from mesper.events.move_rectangle import MoveRectangle
 from mesper.mesper import World
+from mesper.processors.rectangle_mover import RectangleMover
 from mesper.processors.renderer import Renderer
 from mesper.processors.updater import Updater
 from sport.pong import config
-from sport.pong.config import FPS, PADDLE_LEFT_RECT
+from sport.pong.config import FPS, PADDLE_LEFT_RECT, PADDLE_SPEED
 
 
 class Game(World):
@@ -18,10 +23,22 @@ class Game(World):
 
         # Left Paddle Entity
         lp_rectangle = Rectangle(*PADDLE_LEFT_RECT)
-        self.create_entity(lp_rectangle)
+        lp_entity = self.create_entity(lp_rectangle)
+        self.add_component(
+            lp_entity,
+            KeyboardInput(
+                {
+                    pygame.K_UP:
+                        lambda ent, world: world.publish(MoveRectangle(ent, 0, - world.process_dt * PADDLE_SPEED)),
+                    pygame.K_DOWN:
+                        lambda ent, world: world.publish(MoveRectangle(ent, 0, + world.process_dt * PADDLE_SPEED)),
+                }
+            )
+        )
 
         self.add_processor(Renderer(FPS))
         self.add_processor(Updater())
+        self.add_processor(RectangleMover())
 
 
 if __name__ == '__main__':
