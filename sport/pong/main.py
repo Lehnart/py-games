9a78d5d3af8ce095f104935f913149ff6c37d86a
Lehnart <thomasserre63@gmail.com>
2023-02-15 10:53:36 +0100

    Make a paddle sprite that moves

--- sport/pong/main.py
+++ sport/pong/main.py
@@ -3,11 +3,15 @@ import pygame
 from mesper.components.collision import Collision
 from mesper.components.keyboard_input import KeyboardInput
 from mesper.components.rectangle import Rectangle
+from mesper.components.sprite import Sprite
 from mesper.components.window import Window
 from mesper.events.move_rectangle import MoveRectangle
+from mesper.events.move_sprite import MoveSprite
 from mesper.mesper import World
+from mesper.processors.rectangle_collider import RectangleCollider
 from mesper.processors.rectangle_mover import RectangleMover
 from mesper.processors.renderer import Renderer
+from mesper.processors.sprite_mover import SpriteMover
 from mesper.processors.updater import Updater
 from sport.pong import config
 from sport.pong.config import FPS, PADDLE_LEFT_RECT, PADDLE_SPEED
@@ -25,15 +29,16 @@ class Game(World):
         # Left Paddle Entity
         lp_rectangle = Rectangle(*PADDLE_LEFT_RECT)
         lp_collision_rectangle = Collision()
-        lp_entity = self.create_entity(lp_rectangle, lp_collision_rectangle)
+        lp_surface = pygame.Surface(PADDLE_LEFT_RECT[2:4])
+        lp_surface.fill(pygame.Color("white"))
+        lp_sprite = Sprite(lp_surface, PADDLE_LEFT_RECT[0:2])
+        lp_entity = self.create_entity(lp_rectangle, lp_collision_rectangle, lp_sprite)
         self.add_component(
             lp_entity,
             KeyboardInput(
                 {
-                    pygame.K_UP:
-                        lambda ent, world: world.publish(MoveRectangle(ent, 0, - world.process_dt * PADDLE_SPEED)),
-                    pygame.K_DOWN:
-                        lambda ent, world: world.publish(MoveRectangle(ent, 0, + world.process_dt * PADDLE_SPEED)),
+                    pygame.K_UP: self.move_paddle_up,
+                    pygame.K_DOWN: self.move_paddle_down,
                 },
                 is_repeated=True
             )
@@ -42,6 +47,16 @@ class Game(World):
         self.add_processor(Renderer(FPS))
         self.add_processor(Updater())
         self.add_processor(RectangleMover())
+        self.add_processor(RectangleCollider())
+        self.add_processor(SpriteMover())
+
+    def move_paddle_up(self, ent: int, world: World):
+        world.publish(MoveRectangle(ent, 0, - world.process_dt * PADDLE_SPEED))
+        world.publish(MoveSprite(ent, (0, - world.process_dt * PADDLE_SPEED)))
+
+    def move_paddle_down(self, ent: int, world: World):
+        world.publish(MoveRectangle(ent, 0, + world.process_dt * PADDLE_SPEED))
+        world.publish(MoveSprite(ent, (0, + world.process_dt * PADDLE_SPEED)))
 
 
 if __name__ == '__main__':
